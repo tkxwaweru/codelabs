42aea01dc2938f84789b555b05a8468c9c1bec99
Trevor Waweru <trevor.k.x.waweru@gmail.com>
2023-09-25 14:28:04 +0300

    Comment modifications

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import pandas as pd
-#I dont like pandas, the big ones
+
+# I dont like pandas, the big ones
 
 
 # Path to the .xlsx file .
@@ -12,16 +13,18 @@ xlsx_file = "../data/Test Files.xlsx"
 
 df = pd.read_excel(xlsx_file)
 
-df.info()
+# display minimal info of data file
+# df.info()
 
 # Load the student data from the Excel file
-excel_file = 'your_test_file.xlsx'  # Replace with the actual file name
+excel_file = "your_test_file.xlsx"
 df = pd.read_excel(xlsx_file)
+
 
 # Function to generate email addresses
 def generate_email(name):
     # Remove special characters and split the name
-    clean_name = ''.join(e for e in name if e.isalnum() or e.isspace())
+    clean_name = "".join(e for e in name if e.isalnum() or e.isspace())
     name_parts = clean_name.split()
 
     # Extract first and last name (or use what's available)
@@ -37,12 +40,13 @@ def generate_email(name):
 
     return email
 
+
 # Create a dictionary to store email addresses
 email_addresses = {}
 
 # Generate email addresses for each student
 for index, row in df.iterrows():
-    student_name = row['Student Name']
+    student_name = row["Student Name"]
     email = generate_email(student_name)
 
     # Ensure uniqueness
@@ -54,5 +58,3 @@ for index, row in df.iterrows():
 # Print the generated email addresses
 for student_name, email in email_addresses.items():
     print(f"Student Name: {student_name} - Email Address: {email}")
-
- 
